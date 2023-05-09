0a3f7b43f0bf30d8c2e6702b659c9b3e9d2808d0
Dinastiasoft <kayodemolehin@gmail.com>
2023-05-09 14:08:26 +0100

    PageView Widget Added

--- lib/onboarding_screen.dart
+++ lib/onboarding_screen.dart
@@ -1,12 +1,23 @@
 import 'package:flutter/material.dart';
 
-class OnBoardingScreen extends StatelessWidget {
+class OnBoardingScreen extends StatefulWidget {
   const OnBoardingScreen({super.key});
 
   @override
+  State<OnBoardingScreen> createState() => _OnBoardingScreenState();
+}
+
+class _OnBoardingScreenState extends State<OnBoardingScreen> {
+  @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(child: Text('Hello Flutter')),
+    return Scaffold(
+      body: PageView(
+        children: [
+          Container(color: Colors.blue),
+          Container(color: Colors.pink),
+          Container(color: Colors.green),
+        ],
+      ),
     );
   }
 }
